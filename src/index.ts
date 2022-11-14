45fe4085a6cb9aa6e403343f3fe6c54a18ad434b
Kylar13 <ramonpans13@gmail.com>
2022-11-14 17:30:39 +0100

    chore: :bookmark: Release v0.0.3

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { deployCurrentRef } from "./deploy-current-ref";
 import { waitForDeployment } from "./wait-for-deployment";
 
 const run = async () => {
-  core.info(`Starting Deploy to Render workflow`);
+  core.info(`Starting Deploy to Render workflow v0.0.3`);
   try {
     const config = loadConfig();
 
