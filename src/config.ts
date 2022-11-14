c0836b691ce32a53113de65780d1a6b3a3e0b9a1
Kylar13 <ramonpans13@gmail.com>
2022-11-14 17:25:00 +0100

    fix: :bug: Fix issue with naming mismatch

--- src/config.ts
+++ src/config.ts
@@ -11,7 +11,7 @@ export const loadConfig = () => {
     throw new Error("The API Key is missing from the workflow file.");
   }
 
-  const serviceId = core.getInput("apiKserviceIdey");
+  const serviceId = core.getInput("serviceId");
   if (!serviceId) {
     throw new Error("The Service ID is missing from the workflow file.");
   }
